fd94eea4aca0d8cd3f4f5793a169e5ad765e5ece
Audrey <33922203+AudreyRocher@users.noreply.github.com>
2018-07-07 14:22:15 +0200

    Directives error messages

---
+++ invalidmessage.directive.ts
@@ -0,0 +1,47 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appInvalidmessage]'
+})
+export class InvalidmessageDirective implements OnInit, OnDestroy{
+  @Input() invalidmessage: string;
+  control: AbstrctControl;
+  hasView = false;
+  controlValue$: Observable<any>;
+  controlSubscription: Subscription;
+  hasSubmitted: boolean;
+  constructor(
+    private_fg: ControlContainer,
+    private_el: ElementRef,
+    private render: Renderer2
+  ) { }
+
+  ngOnInit() {
+    this.control = this.form.get(this.invalidmessage);
+    let formSubmit$ = (<FormGroupDirective>this._fg).ngSubmit.map(() =>{
+      this.hasSubmitted = true;
+    });
+  }
+
+  private setVisible() {
+    if(this.control.invalid && (this.control.dirty || this.hasSubmitted)) {
+      this.render.removeStyle(this._el.nativeElement, 'display');
+    }else {
+      this.render.setStyle(this._el.nativeElement, 'display', 'none');
+    }
+    match(error:string){
+      if(this.control && this.control.errors){
+        if(Object.keys(this.control.errors).indexOf(error) > -1){
+          return true;
+        }
+      }
+      return false;
+    }
+
+    get form() { return this._fg_formDirective ? (this._fg.formDirective as FormGroupDirective).form : null; }
+
+    ngOnDestroy(){
+      this.controlSubscription.unsubscribe();
+    }
+  }
+}
---
+++ invalidtype.directive.spec.ts
@@ -0,0 +1,8 @@
+import { InvalidtypeDirective } from './invalidtype.directive';
+
+describe('InvalidtypeDirective', () => {
+  it('should create an instance', () => {
+    const directive = new InvalidtypeDirective();
+    expect(directive).toBeTruthy();
+  });
+});
